773ad50bf8f6fe94854d239d3419130f8cf91c49
驼驼 <153578087+lkjhgfdsamnbvcxzpoi@users.noreply.github.com>
2024-07-09 20:53:44 +0800

    Update Paint.cpp

--- CODE/Paint.cpp
+++ CODE/Paint.cpp
@@ -4,7 +4,7 @@
 // 绘制类型一直方图
 QChart *Paint::createBarChart1()
 {
-    void getnums1();
+     getnums1(data1);
     QChart *chart = new QChart();
     chart->setTitle("直方图演示");
 
@@ -39,46 +39,43 @@ QChart *Paint::createBarChart1()
     return chart;
 }
 
-
-void Paint:: getnums1(){
-    sort(data.begin(),data.end());
-    int i=0;
-    data1[0]=0;
-    while(data[i]<90&&i<data.size()){
-        data1[0]++;
-    }
-    data1[1]=0;
-    while(data[i]<100&&data[i]>=90&&i<data.size()){
-        data1[1]++;
-    }
-    data1[2]=0;
-    while(data[i]<110&&data[i]>=100&&i<data.size()){
-        data1[2]++;
-    }
-    data1[3]=0;
-    while(data[i]<120&&data[i]>=110&&i<data.size()){
-        data1[3]++;
-    }
-    data1[4]=0;
-    while(data[i]<130&&data[i]>=120&&i<data.size()){
-        data1[4]++;
-    }
-    data1[5]=0;
-    while(data[i]<140&&data[i]>=130&&i<data.size()){
-        data1[5]++;
-    }
-    data1[6]=0;
-    while(data[i]<=150&&data[i]>=140&&i<data.size()){
-        data1[6]++;
-    }
+bool comp0(int x){
+    return x<90;
+}
+bool comp1(int x){
+    return (x>=90&&x<100);
+}
+bool comp2(int x){
+    return (x>=100&&x<110);
+}
+bool comp3(int x){
+    return (x>=110&&x<120);
+}
+bool comp4(int x){
+    return (x>=120&&x<130);
+}
+bool comp5(int x){
+    return (x>=130&&x<140);
+}
+bool comp6(int x){
+    return (x>=140&&x<=150);
+}
+void Paint:: getnums1(int*&Data){
+    Data[0]=count_if(data.begin(),data.end(),comp0);
+    Data[1]=count_if(data.begin(),data.end(),comp1);
+    Data[2]=count_if(data.begin(),data.end(),comp2);
+    Data[3]=count_if(data.begin(),data.end(),comp3);
+    Data[4]=count_if(data.begin(),data.end(),comp4);
+    Data[5]=count_if(data.begin(),data.end(),comp5);
+    Data[6]=count_if(data.begin(),data.end(),comp6);
 }
 QChart *Paint::createBarChart2()
 {
-    void getnums2();
+     getnums2(data2);
     QChart *chart = new QChart();
     chart->setTitle("直方图演示");
 
-    QBarSet *set0 = new QBarSet("成绩");
+    QBarSet *set0 = new QBarSet("人数");
     *set0<<data2[0]<<data2[1]<<data2[2]<<data2[3]<<data2[4];
 
     QBarSeries  *series = new QBarSeries (chart);
@@ -93,7 +90,7 @@ QChart *Paint::createBarChart2()
     axis->append(categories);
     chart->createDefaultAxes();
     chart->setAxisX(axis, series);
-    chart->axes(Qt::Vertical).first()->setRange(0,5000);
+    chart->axes(Qt::Vertical).first()->setRange(0,500);//设置纵轴范围
 
     // 在标签和轴之间加空格
     QValueAxis *axisY = qobject_cast<QValueAxis*>(chart->axes(Qt::Vertical).first());
@@ -108,29 +105,30 @@ QChart *Paint::createBarChart2()
 
     return chart;
 }
-
-
-void Paint:: getnums2(){
-    sort(data.begin(),data.end());
-    int i=0;
-    data2[0]=0;
-    while(data[i]<60&&i<data.size()){
-        data2[0]++;
-    }
-    data2[1]=0;
-    while(data[i]<70&&data[i]>=60&&i<data.size()){
-        data2[1]++;
-    }
-    data2[2]=0;
-    while(data[i]<80&&data[i]>=70&&i<data.size()){
-        data2[2]++;
-    }
-    data2[3]=0;
-    while(data[i]<90&&data[i]>=80&&i<data.size()){
-        data2[3]++;
-    }
-    data2[4]=0;
-    while(data[i]<=100&&data[i]>=90&&i<data.size()){
-        data2[4]++;
-    }
+bool comp7(int x){
+    return x<60;
 }
+bool comp8(int x){
+    return (x>=60&&x<70);
+}
+bool comp9(int x){
+    return (x>=70&&x<80);
+}
+
+bool comp10(int x){
+    return (x>=80&&x<90);
+}
+bool comp11(int x){
+    return (x>=90&&x<=100);
+}
+
+
+void Paint:: getnums2(int*&Data){
+    Data[0]=count_if(data.begin(),data.end(),comp7);
+    Data[1]=count_if(data.begin(),data.end(),comp8);
+    Data[2]=count_if(data.begin(),data.end(),comp9);
+    Data[3]=count_if(data.begin(),data.end(),comp10);
+    Data[4]=count_if(data.begin(),data.end(),comp11);
+
+}
+
