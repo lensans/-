fa02b57cea192db85f0aefc7be0d87e171c50fd5
驼驼 <153578087+lkjhgfdsamnbvcxzpoi@users.noreply.github.com>
2024-07-09 20:52:05 +0800

    Update Paint.h

--- CODE/Paint.h
+++ CODE/Paint.h
@@ -5,15 +5,17 @@
 using namespace std;
 class Paint{
 public:
-    Paint(vector<int>data):data(data){}
+    Paint(vector<int>data):data(data){
+        data1=new int[7];
+        data2=new int[5];
+        }
     QChart *createBarChart1();
     QChart *createBarChart2();
-    void getnums1();
-    void getnums2();
+    void getnums1(int*&Data);
+    void getnums2(int*&Data);
 private:
     vector<int>data;
-    int data1[7];
-    int data2[5];
+    int *data1;
+    int *data2;
 };
-
 #endif // PAINT_H
