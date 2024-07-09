226e6e65b9d651de86bb7aa81f5e1b17e5080fa0
Gawiiiii <136977336+Gawiiiii@users.noreply.github.com>
2024-07-09 20:25:37 +0800

    更新course,grade,manager,student头文件

--- CODE/course.h
+++ CODE/course.h
@@ -1,4 +1,17 @@
 #ifndef COURSE_H
 #define COURSE_H
 
+#include <string>
+
+class Course {
+private:
+    int courseId;
+    std::string courseName;
+
+public:
+    Course(int id, const std::string& name);
+    QString getCourseId() const;
+    std::string getCourseName() const;
+};
+
 #endif // COURSE_H
--- CODE/manager.h
+++ CODE/manager.h
@@ -1,4 +1,24 @@
 #ifndef MANAGER_H
 #define MANAGER_H
 
+#include <vector>
+#include "student.h"
+#include "course.h"
+#include "grade.h"
+
+class Manager {
+private:
+    std::vector<Student> students;
+    std::vector<Course> courses;
+
+public:
+    void addStudent(const Student& student);
+    void addCourse(const Course& course);
+    void addGrade(int studentId, const Grade& grade);
+    Student* findStudent(int studentId);
+    Course* findCourse(int courseId);
+    std::vector<Student> getAllStudents() const;
+    std::vector<Course> getAllCourses() const;
+};
+
 #endif // MANAGER_H
