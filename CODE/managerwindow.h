aa95f4879bbba1dcd362468fba590ba7cc207afb
小鲸鱼 <153498773+xiaojingyuba@users.noreply.github.com>
2024-07-11 00:36:08 +0800

    Update managerwindow.h

--- CODE/managerwindow.h
+++ CODE/managerwindow.h
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 #ifndef MANAGERWINDOW_H
 #define MANAGERWINDOW_H
-=======
-﻿#ifndef MANAGERWINDOW_H
-#define MANAGERWINDOW_H
-
->>>>>>> eae3de45a9ca010dc73b35dbb34bfc4ba3df93c4
-#include "input_score.h"
 #include <QMainWindow>
-#include <QWidget>
-#include <QObject>
 
 namespace Ui {
 class managerwindow;
@@ -20,10 +11,14 @@ class managerwindow : public QMainWindow
     Q_OBJECT
 public:
     explicit managerwindow(QWidget *parent = nullptr);
-    void on_btn_input_clicked();
-    void on_btn_revise_clicked();
-    void on_btn_check_clicked();
-    //void on_btn_total_clicked();
+    
+    void on_btn_input_clicked();//录入成绩
+    void on_btn_revise_clicked();//修改成绩
+    void on_btn_check_clicked();  //查询成绩
+    void on_btn_total_clicked();//总分排名
+    void on_btn_subject_clicked();//学科间线性关系
+    void on_btn_single_clicked();//单科成绩直方图
+    
     ~managerwindow();
 
 private:
