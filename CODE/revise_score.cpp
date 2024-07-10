578a102b26af212def8f68e8e0fd20909a9a8c49
小鲸鱼 <153498773+xiaojingyuba@users.noreply.github.com>
2024-07-10 18:23:41 +0800

    Update revise_score.cpp

--- CODE/revise_score.cpp
+++ CODE/revise_score.cpp
@@ -8,7 +8,7 @@ revise_score::revise_score(QWidget *parent)
 {
     ui->setupUi(this);
 
-    connect(ui->btn_revise_return,&QPushButton::clicked,this,&revise_score::on_btn_revise_return_clicked);//点击返回按钮，跳入菜单界面
+    //connect(ui->btn_revise_return,&QPushButton::clicked,this,&revise_score::on_btn_revise_return_clicked);//点击返回按钮，跳入菜单界面
     connect(ui->btn_revise_return,&QPushButton::clicked,this,&revise_score::close);//并且关闭录入成绩界面
 
 }
