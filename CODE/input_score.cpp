3b88787a4f66a7d38591ba5c5e37f69fef1d7328
小鲸鱼 <153498773+xiaojingyuba@users.noreply.github.com>
2024-07-11 00:30:04 +0800

    Update input_score.cpp

--- CODE/input_score.cpp
+++ CODE/input_score.cpp
@@ -1,9 +1,6 @@
-﻿#include "input_score.h"
+#include "input_score.h"
 #include "ui_input_score.h"
-#include "managerwindow.h"
-#include "mysql_connect.h"
-
-
+#include <mysql_connect.h>
 
 Input_score::Input_score(QWidget *parent)
     : QMainWindow(parent)
@@ -11,35 +8,22 @@ Input_score::Input_score(QWidget *parent)
 {
     ui->setupUi(this);
 
-    //connect(ui->input_single_return,&QPushButton::clicked,this,&Input_score::on_btn_single_return_clicked);//点击返回按钮，跳入菜单界面
-    connect(ui->input_single_return,&QPushButton::clicked,this,&Input_score::close);//并且关闭录入成绩界面
+    connect(ui->input_single_return,&QPushButton::clicked,this,&Input_score::close);//单个录入时点击返回，关闭录入成绩界面
 
-    //connect(ui->input_excel_return,&QPushButton::clicked,this,&Input_score::on_btn_excel_return_clicked);//点击返回按钮，跳入菜单界面
-    connect(ui->input_excel_return,&QPushButton::clicked,this,&Input_score::close);//并且关闭录入成绩界面
+    connect(ui->input_excel_return,&QPushButton::clicked,this,&Input_score::close);//EXCEL导入时点击返回，关闭录入成绩界面
 
-    connect(ui->input_single_confirm,&QPushButton::clicked,this,&Input_score::on_input_single_confirm_clicked);
+    connect(ui->input_single_confirm,&QPushButton::clicked,this,&Input_score::on_input_single_confirm_clicked);//单个录入时点击确认录入
 }
 
-void Input_score::on_btn_single_return_clicked()
+void Input_score::on_input_single_confirm_clicked()//单个录入成绩
 {
-    managerwindow *ma_in_on1 = new managerwindow(this);
-    ma_in_on1->show();
-}
-
-void Input_score::on_btn_excel_return_clicked()
-{
-    managerwindow *ma_in_on2 = new managerwindow(this);
-    ma_in_on2->show();
-}
-
-void Input_score::on_input_single_confirm_clicked()
-{
-    //Init();
+    //从文本框读入要查询的学生姓名和id
     QString student_name = ui->input_name_line->text();
     QString student_id = ui->input_num_line->text();
 
     VP subject_scores(7);
-
+    
+    //从文本框读入要查询的学科和对应成绩
     subject_scores[0].first = ui->input_Chinese->text();
     subject_scores[0].second = ui->input_Chinese_line->text().toInt();
     subject_scores[1].first = ui->input_math->text();
@@ -56,9 +40,8 @@ void Input_score::on_input_single_confirm_clicked()
     subject_scores[6].second = ui->input_sum_line->text().toInt();    
 
     DB db;
-    
     db.add_score(student_id, student_name, subject_scores);
-   
+
 }
 
 Input_score::~Input_score()
