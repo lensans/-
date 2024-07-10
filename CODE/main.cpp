efba53288f5ca5c5d63fad3c889e0ae40fafbdde
小鲸鱼 <153498773+xiaojingyuba@users.noreply.github.com>
2024-07-10 18:21:11 +0800

    Update main.cpp

--- CODE/main.cpp
+++ CODE/main.cpp
@@ -7,8 +7,8 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    login w1;
-    w1.show();
+    //login w1;
+    //w1.show();
     managerwindow w2;
     w2.show();
     return a.exec();
