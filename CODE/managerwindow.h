54e35518ff035ee32ab815ed8a5cca6c6359578f
小鲸鱼 <153498773+xiaojingyuba@users.noreply.github.com>
2024-07-10 18:22:30 +0800

    Update managerwindow.h

--- CODE/managerwindow.h
+++ CODE/managerwindow.h
@@ -1,8 +1,6 @@
-#ifndef MANAGERWINDOW_H
-#define MANAGERWINDOW_H
-
+#include "input_score.h"
 #include <QMainWindow>
-
+#include <QWidget>>
 namespace Ui {
 class managerwindow;
 }
@@ -12,10 +10,15 @@ class managerwindow : public QMainWindow
     Q_OBJECT
 public:
     explicit managerwindow(QWidget *parent = nullptr);
+    void on_btn_input_clicked();
+    void on_btn_revise_clicked();
+    void on_btn_check_clicked();
+    //void on_btn_total_clicked();
     ~managerwindow();
 
 private:
     Ui::managerwindow *ui;
+
 };
 
 #endif // MANAGERWINDOW_H
