2acaa26c42db037eceed835ae5e5e104bf4e17a6
tuotuo <153578087+TuoTuo-pro117@users.noreply.github.com>
2024-07-13 11:47:54 +0800

    Update single_score.h

--- CODE/single_score.h
+++ CODE/single_score.h
@@ -15,6 +15,19 @@ public:
     explicit Single_score(QWidget *parent = nullptr);
     ~Single_score();
 
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_pushButton_3_clicked();
+
+    void on_pushButton_4_clicked();
+
+    void on_pushButton_5_clicked();
+
+    void on_pushButton_6_clicked();
+
 private:
     Ui::Single_score *ui;
 };
