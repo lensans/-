1fba266b0753fe348c2043ed92e4f059c594397d
tuotuo <153578087+TuoTuo-pro117@users.noreply.github.com>
2024-07-13 11:44:38 +0800

    Add files via upload

---
+++ CODE/cmepaints.cpp
@@ -0,0 +1,35 @@
+#include "cmepaints.h"
+#include "ui_cmepaints.h"
+#include"paint.h"
+#include"single_score.h"
+#include"mysql_connect.h"
+#include"single_score.h"
+CMEpaints::CMEpaints(QString subject,QWidget *parent)
+    : QWidget(parent),subject(subject)
+    , ui(new Ui::CMEpaints)
+{
+    ui->setupUi(this);
+     extern DB db;
+    vector<int>score;
+     int i = 1;
+     QString s = QString::number(i);
+     int j=1000;
+     QString s2=QString::number(j);
+     db.get_students_scores(s,s2,subject,score);
+     Paint a(score,subject);
+     QChartView *chartView;
+     chartView=new QChartView(a.createBarChart1());
+     ui->verticalLayout->insertWidget(0,chartView);
+}
+
+CMEpaints::~CMEpaints()
+{
+    delete ui;
+}
+
+void CMEpaints::on_pushButton_clicked()
+{   this->close();
+    Single_score*pic=new Single_score();
+    pic->show();
+}
+
