e5fe0d5aac97725d42942715b81fb229959d534a
tuotuo <153578087+TuoTuo-pro117@users.noreply.github.com>
2024-07-13 11:48:38 +0800

    Update single_score.cpp

--- CODE/single_score.cpp
+++ CODE/single_score.cpp
@@ -1,6 +1,7 @@
 #include "single_score.h"
 #include "ui_single_score.h"
-
+#include"cmepaints.h"
+#include"pcbpaints.h"
 Single_score::Single_score(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Single_score)
@@ -12,3 +13,57 @@ Single_score::~Single_score()
 {
     delete ui;
 }
+
+void Single_score::on_pushButton_clicked()
+{
+    this->close();
+    QString subject="Chinese";
+    CMEpaints*pic=new CMEpaints(subject);
+    pic->show();
+}
+
+
+void Single_score::on_pushButton_2_clicked()
+{
+    this->close();
+    QString subject="Math";
+    CMEpaints*pic=new CMEpaints(subject);
+    pic->show();
+}
+
+
+void Single_score::on_pushButton_3_clicked()
+{
+    this->close();
+    QString subject="English";
+    CMEpaints*pic=new CMEpaints(subject);
+    pic->show();
+}
+
+void Single_score::on_pushButton_4_clicked()
+{
+    this->close();
+    QString subject="Physics";
+    PCBpaints*pic=new PCBpaints(subject);
+    pic->show();
+}
+
+
+void Single_score::on_pushButton_5_clicked()
+{
+    this->close();
+    QString subject="Chemestry";
+    PCBpaints*pic=new PCBpaints(subject);
+    pic->show();
+}
+
+
+
+void Single_score::on_pushButton_6_clicked()
+{
+    this->close();
+    QString subject="Biology";
+    PCBpaints*pic=new PCBpaints(subject);
+    pic->show();
+}
+
