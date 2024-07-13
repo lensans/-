4cb4f4d1a8b92ef7b4722d167320439c24d6dc00
tuotuo <153578087+TuoTuo-pro117@users.noreply.github.com>
2024-07-13 11:51:47 +0800

    Update subject_relation.cpp

--- CODE/subject_relation.cpp
+++ CODE/subject_relation.cpp
@@ -1,14 +1,39 @@
 #include "subject_relation.h"
 #include "ui_subject_relation.h"
-
+#include<QtCharts>
+#include"Relationship.h"
+#include <mysql_connect.h>
+#include"teacherwindow.h"
 Subject_relation::Subject_relation(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Subject_relation)
 {
     ui->setupUi(this);
+   extern DB db;
+    int i = 1;
+    QString s = QString::number(i);
+    int j=1000;
+    QString s2=QString::number(j);
+    QString subject1="CME";
+    QString subject2="PCB";
+    vector<int>cmescore;
+    vector<int>pcbscore;
+    db.get_students_scores(s,s2,subject1,cmescore);
+    db.get_students_scores(s,s2,subject2,pcbscore);
+    Relationship A(cmescore,pcbscore);
+    ui->graphicsView->setChart(A.getrelationshipmap());
+    ui->mainlineEdit->setText(A.getrelationship());
 }
 
 Subject_relation::~Subject_relation()
 {
     delete ui;
 }
+
+void Subject_relation::on_pushButton_clicked()
+{
+    this->close();
+    TeacherWindow*father=new TeacherWindow();
+    father->show();
+}
+
