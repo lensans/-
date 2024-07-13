d917b7feb1491d2d9abdcd61ec2ecd60ecc75c3b
tuotuo <153578087+TuoTuo-pro117@users.noreply.github.com>
2024-07-13 17:04:53 +0800

    Update teacherwindow.h

--- CODE/teacherwindow.h
+++ CODE/teacherwindow.h
@@ -17,11 +17,13 @@ public:
     void on_btn_teacher_check_clicked();  //查询成绩
     void on_btn_teacher_revise_password_clicked();//重置密码
     void on_btn_teacher_total_clicked();//一分一段表
-    void on_btn_teacher_subject_clicked();//学科间线性关系
     void on_btn_teacher_single_clicked();//单科成绩直方图
 
 
     ~TeacherWindow();
+
+private slots:
+    void on_btn_teacher_subject_relation_clicked();
 
 private:
     Ui::TeacherWindow *ui;
