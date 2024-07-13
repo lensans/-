90b7e7749d59db29f40f1079e9be8c68347fea31
tuotuo <153578087+TuoTuo-pro117@users.noreply.github.com>
2024-07-13 11:38:07 +0800

    Add files via upload

---
+++ CODE/cmepaints.h
@@ -0,0 +1,29 @@
+#ifndef CMEPAINTS_H
+#define CMEPAINTS_H
+
+#include <QWidget>
+#include<QString>
+using namespace std;
+namespace Ui {
+class CMEpaints;
+}
+
+class CMEpaints : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit CMEpaints(QString subject,QWidget *parent = nullptr);
+    ~CMEpaints();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::CMEpaints *ui;
+    QString subject;
+
+
+};
+
+#endif // CMEPAINTS_H
